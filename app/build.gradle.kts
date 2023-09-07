316a5d289202b34fb5a30dcaa8dd16ebbb5b3b19
JoaoPMA23 <jpmartinsandrade123@gmail.com>
2023-09-06 22:19:10 -0300

    Arrumamo erro da dependencia 29/08/2023

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,15 +41,15 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
     implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
     implementation("com.google.firebase:firebase-analytics-ktx")
-    implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
-    implementation("com.google.firebase:firebase-auth:12.0.0")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
+    implementation("com.google.firebase:firebase-auth:22.1.1")
     implementation("com.google.firebase:firebase-database-ktx:20.2.2")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/teste/LoginActivity.kt
+++ app/src/main/java/com/example/teste/LoginActivity.kt
@@ -1,11 +1,29 @@
 package com.example.teste
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.core.view.View
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity(
+    var auth: FirebaseAuth,
+    var c1 : EditText,
+    val c2 : EditText
+) : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        c1 = findViewById<EditText>(androidx.core.R.id.edit_text_id)
+        auth = FirebaseAuth.getInstance()
     }
+
+    fun login(v: View){
+
+    }
+
+
 }
