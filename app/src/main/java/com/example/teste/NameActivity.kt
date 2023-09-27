3a8da07f2a090045fa50dbcec29205e59fed8616
JoaoPMA23 <jpmartinsandrade123@gmail.com>
2023-09-27 12:45:48 -0300

    PasswordActivity e NameActivity 6 video 3:05 - JP

--- app/src/main/java/com/example/teste/NameActivity.kt
+++ app/src/main/java/com/example/teste/NameActivity.kt
@@ -4,8 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class NameActivity : AppCompatActivity() {
+    lateinit var email: String
+    lateinit var password: String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
+        val myIntent = intent
+        if(myIntent != null){
+            val email = myIntent.getStringExtra("email")
+
+            val password = myIntent.getStringExtra("password")
+        }
     }
 }
