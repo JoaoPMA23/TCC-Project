25b2f2aea0b14ce96d0248f371ff667c3188d12b
JoaoPMA23 <jpmartinsandrade123@gmail.com>
2023-09-26 22:42:19 -0300

    PasswordActivity e NameActivity 5 video 14:19 - JP

---
+++ app/src/main/java/com/example/teste/PasswordActivity.kt
@@ -0,0 +1,33 @@
+package com.example.teste
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.database.core.view.View
+
+class PasswordActivity(
+    private var email: String,
+    private val e3_password: EditText
+) : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_password)
+        e3_password.findViewById<EditText>(R.id.editTextTextPassword)
+        val myIntent: Intent = intent
+        email = myIntent.getStringExtra("email").toString()
+    }
+
+    fun goToNamePicActivity(v: View) {
+        if(e3_password.text.toString().length >6){
+            val myIntent = Intent(this@PasswordActivity, NameActivity::class.java)
+            myIntent.putExtra("email", email)
+            myIntent.putExtra("password", e3_password.text.toString())
+            startActivity(myIntent)
+        }
+        else{
+            Toast.makeText(applicationContext, "O tamanho da senha deverá ser maior que 6 caracteres", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
--- app/src/main/java/com/example/teste/RegisterActivity.kt
+++ app/src/main/java/com/example/teste/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.teste
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -7,6 +8,8 @@ import android.widget.AbsListView
 import android.widget.EditText
 import android.widget.ProgressBar
 import android.widget.Toast
+import android.content.Context
+import android.content.Intent
 
 import com.google.firebase.auth.FirebaseAuth
 
@@ -16,12 +19,13 @@ class RegisterActivity(
     var cadtel: EditText,
     var check: Boolean = false,
     private var auth: FirebaseAuth,
-    //var progressbar: ProgressBar = ProgressBar(this@RegisterActivity)
+    private var dialog: ProgressDialog
 
 
 ) : AppCompatActivity() {
-
     lateinit var progressbar: ProgressBar
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -29,6 +33,7 @@ class RegisterActivity(
         cadsenha = findViewById<EditText>(R.id.txtSenhaCadastro)
         cadtel = findViewById<EditText>(R.id.txtTelefoneCadastro)
         auth = FirebaseAuth.getInstance()
+        dialog = ProgressDialog(this)
     }
 
 
@@ -38,13 +43,18 @@ class RegisterActivity(
 
         auth.fetchSignInMethodsForEmail(cademail.text.toString()).addOnCompleteListener { task ->
             if (task.isSuccessful) {
+                dialog.dismiss()
                 val signInMethods = task.result?.signInMethods
                 val check = signInMethods?.isNotEmpty() ?: false
 
                 if (!check) {
+                    val myIntent = Intent(this@RegisterActivity, PasswordActivity::class.java)
+                    myIntent.putExtra("email", cademail.text.toString())
+                    startActivity(myIntent)
                     // O email não está cadastrado
                 } else {
                     // O email já está cadastrado
+                    dialog.dismiss()
                     Toast.makeText(applicationContext, "BURRÃO JÁ ESTÁ CADASTRADO", Toast.LENGTH_SHORT).show()
                 }
             } else {
